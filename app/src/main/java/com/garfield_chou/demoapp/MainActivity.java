8ecf5bf5ff133ab4b98f2e2942aa08f499536d84
garfield_chou <garfield_chou@asus.com>
2016-12-08 17:36:45 +0800

    toast for input message

--- app/src/main/java/com/garfield_chou/demoapp/MainActivity.java
+++ app/src/main/java/com/garfield_chou/demoapp/MainActivity.java
@@ -2,8 +2,20 @@ package com.garfield_chou.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction (View view){
+
+        Log.i("Info", "Button Pressed!");
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Toast.makeText(MainActivity.this, "Hi, " + nameEditText.getText().toString(), Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
